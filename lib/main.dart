7fe61f5cb8cf80fda9528a63cca3051557dd7296
Prabhneet Narula <imailcode@gmail.com>
2022-01-20 08:14:31 +0530

    FrontPage Completed.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: StarterPage());
+    return MaterialApp(
+        theme: ThemeData(fontFamily: 'Roboto'), home: StarterPage());
   }
 }
--- lib/pages/starterPage.dart
+++ lib/pages/starterPage.dart
@@ -10,6 +10,65 @@ class StarterPage extends StatefulWidget {
 class _StarterPageState extends State<StarterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("lib/assets/images/starter.jpg"),
+              fit: BoxFit.cover),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.bottomCenter, colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.8),
+            Colors.black.withOpacity(0.2)
+          ])),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Taking Order for Delivery!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                const Text("See restaurants nearby by \nadding the location",
+                    style: TextStyle(
+                        color: Colors.white, height: 1.4, fontSize: 18)),
+                const SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient: const LinearGradient(
+                          colors: [Colors.yellow, Colors.orange])),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    child: const Text(
+                      "Start",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    onPressed: () => {},
+                  ),
+                ),
+                const SizedBox(height: 30),
+                const Align(
+                  child: Text(
+                    "Now Delivery To Your Door 24/7",
+                    style: TextStyle(color: Colors.white70, fontSize: 15),
+                  ),
+                ),
+                const SizedBox(height: 20)
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
