95f7c1c8a4d6ea66744c45bf664015bf84c79641
Prabhneet Narula <imailcode@gmail.com>
2022-01-20 09:16:01 +0530

    FadeAnimation Added.

---
+++ lib/animations/fadeAnimation.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:simple_animations/multi_tween/multi_tween.dart';
+import 'package:simple_animations/stateless_animation/play_animation.dart';
+
+enum AniProps { opacity, translateX }
+
+class FadeAnimation extends StatelessWidget {
+  final double delay;
+  final Widget child;
+
+  FadeAnimation(this.delay, this.child);
+
+  @override
+  Widget build(BuildContext context) {
+    final tween = MultiTween<AniProps>()
+      ..add(AniProps.opacity, Tween(begin: 0.0, end: 1.0),
+          const Duration(milliseconds: 500))
+      ..add(AniProps.translateX, Tween(begin: 150.0, end: 0.0),
+          const Duration(microseconds: 500), Curves.easeOut);
+    return PlayAnimation<MultiTweenValues<AniProps>>(
+        delay: Duration(milliseconds: (500 * delay).round()),
+        duration: tween.duration,
+        tween: tween,
+        child: child,
+        builder: (context, child, value) => Opacity(
+              opacity: value.get(AniProps.opacity),
+              child: Transform.translate(
+                  offset: Offset(0, value.get(AniProps.translateX)),
+                  child: child),
+            ));
+  }
+}
--- lib/pages/starterPage.dart
+++ lib/pages/starterPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/animations/fadeAnimation.dart';
 
 class StarterPage extends StatefulWidget {
   const StarterPage({Key? key}) : super(key: key);
@@ -30,37 +31,52 @@ class _StarterPageState extends State<StarterPage> {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  "Taking Order for Delivery!",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold),
+                FadeAnimation(
+                  1,
+                  const Text(
+                    "Taking Order for Delivery!",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
                 const SizedBox(height: 20),
-                const Text("See restaurants nearby by \nadding the location",
-                    style: TextStyle(
-                        color: Colors.white, height: 1.4, fontSize: 18)),
+                FadeAnimation(
+                    1.3,
+                    const Text(
+                        "See restaurants nearby by \nadding the location",
+                        style: TextStyle(
+                            color: Colors.white, height: 1.4, fontSize: 18))),
                 const SizedBox(height: 20),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      gradient: const LinearGradient(
-                          colors: [Colors.yellow, Colors.orange])),
-                  child: MaterialButton(
-                    minWidth: double.infinity,
-                    child: const Text(
-                      "Start",
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+                FadeAnimation(
+                  1.6,
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        gradient: const LinearGradient(
+                            colors: [Colors.yellow, Colors.orange])),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      child: FadeAnimation(
+                        1.8,
+                        const Text(
+                          "Start",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                      onPressed: () => {},
                     ),
-                    onPressed: () => {},
                   ),
                 ),
                 const SizedBox(height: 30),
-                const Align(
-                  child: Text(
-                    "Now Delivery To Your Door 24/7",
-                    style: TextStyle(color: Colors.white70, fontSize: 15),
+                Align(
+                  child: FadeAnimation(
+                    2,
+                    const Text(
+                      "Now Delivery To Your Door 24/7",
+                      style: TextStyle(color: Colors.white70, fontSize: 15),
+                    ),
                   ),
                 ),
                 const SizedBox(height: 20)
